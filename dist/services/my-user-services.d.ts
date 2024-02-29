344d0f0530e14b21ef30076e74c10e25799ce4f5
ashank kumar kushwaha <ash.22kk@gmail.com>
2024-03-01 00:41:28 +0530

    ashank2

--- dist/services/my-user-services.d.ts
+++ dist/services/my-user-services.d.ts
@@ -1,10 +1,12 @@
 import { UserService, TokenService } from '@loopback/authentication';
 import { Userdetails } from '../models';
 import { UserdetailsRepository } from '../repositories';
+import { UserProfile } from '@loopback/security';
 export declare class MyService implements UserService<Userdetails, Userdetails> {
     private userRepository;
     private tokenService;
     constructor(userRepository: UserdetailsRepository, tokenService: TokenService);
+    convertToUserProfile(user: Userdetails): UserProfile;
     generateToken(user: Userdetails): Promise<string>;
     verifyToken(token: string): Promise<Userdetails>;
     verifyCredentials(credentials: Userdetails): Promise<Userdetails>;
